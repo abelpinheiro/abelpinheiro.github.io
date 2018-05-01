7f90d9b9fda96149c01e92aa90b9c65111ceda25
Abel Pinheiro de Figueiredo <abelpinheiro95@gmail.com>
2018-05-01 12:42:43 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,12 +1,2 @@
 import Rythm from 'rythm.js'
 confirm("Hey Hey Hey start dash!");
-
-import Rythm from 'rythm.js'
-const rythm = new Rythm();
-rythm.setMusic("C:\Users\Abel\Downloads\Maybe the next waltz.mp3");
-rythm.start();
-
-rythm.addRythm('color2', 'color', 0, 10, {
-              from: [0,0,255],
-              to:[255,0,255]
-            })
